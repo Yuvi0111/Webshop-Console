3eb152f618cc83fc1b701fa65c3166be76021a1a
Yuvi <tamstamanna67@gmail.com>
2023-10-24 21:48:43 +0530

    Started
    Started

--- Program.cs
+++ Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine
+            Console.WriteLine("--------- \n Login Screen \n ---------");
+            Console.WriteLine("1. Sign up \n 2. Sign in \n 3. Exit");
+            Console.WriteLine("Note: Enter the number to choose" +
+                "your function throughout the program");
+            Console.Write("Choose your function: ");
+            string loginInput = Console.ReadLine();
+
+            while (loginInput != "3") ;
+            { if (loginInput == "1")
+                    SignUpScreen();
+              else if (loginInput == "2")
+                    Placeholder
+              else 
+
+
+            }
         }
+
+        static void SignUpScreen()
+        {
+            Object User1 = new Object();
+            Console.WriteLine("--------- \n Sign in Menu \n ---------");
+            Console.WriteLine("");
+            
+            Console.Write("Enter your Email: ");
+            User1.Email = Console.ReadLine();
+
+            Console.Write("Enter your Username: ");
+            User1.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter your Phone Number: ");
+            User1.phoneNumber = Convert.ToInt32(Console.ReadLine());
+        }
+       
     }
+
+   public class Object
+   {
+      public String Email, Name;
+     public int phoneNumber;
+        
+   }
 }

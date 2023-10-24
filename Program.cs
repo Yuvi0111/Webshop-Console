45c99a1b26b1614a7c02f4f6afaf9564030d82c6
Yuvi <tamstamanna67@gmail.com>
2023-10-24 20:55:53 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,9 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-
-            Console.WriteLine("Hello Ara");
+            Console.WriteLine
         }
     }
 }
